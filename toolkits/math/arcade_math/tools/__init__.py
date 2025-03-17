cd7eca306ae1cd5fc1d6bef38a396f86fd6890d0
Eric Gustin <34000337+EricGustin@users.noreply.github.com>
2025-03-17 11:58:17 -0800

    Release Arcade Math 1.0.0 (#300)

---
+++ toolkits/math/arcade_math/tools/__init__.py
@@ -0,0 +1,65 @@
+from arcade_math.tools.arithmetic import (
+    add,
+    divide,
+    mod,
+    multiply,
+    subtract,
+    sum_list,
+    sum_range,
+)
+from arcade_math.tools.exponents import (
+    log,
+    power,
+)
+from arcade_math.tools.miscellaneous import (
+    abs_val,
+    factorial,
+    sqrt,
+)
+from arcade_math.tools.random import (
+    generate_random_float,
+    generate_random_int,
+)
+from arcade_math.tools.rational import (
+    gcd,
+    lcm,
+)
+from arcade_math.tools.rounding import (
+    ceil,
+    floor,
+    round_num,
+)
+from arcade_math.tools.statistics import (
+    avg,
+    median,
+)
+from arcade_math.tools.trigonometry import (
+    deg_to_rad,
+    rad_to_deg,
+)
+
+__all__ = [
+    "add",
+    "subtract",
+    "multiply",
+    "divide",
+    "sum_list",
+    "sum_range",
+    "mod",
+    "log",
+    "power",
+    "abs_val",
+    "factorial",
+    "sqrt",
+    "generate_random_float",
+    "generate_random_int",
+    "gcd",
+    "lcm",
+    "ceil",
+    "floor",
+    "round_num",
+    "avg",
+    "median",
+    "deg_to_rad",
+    "rad_to_deg",
+]
